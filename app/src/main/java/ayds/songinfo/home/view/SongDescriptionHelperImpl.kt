8eb5a425f695d0203a312a5a69bbe2f1d55d2791
Gonzalo Piñeiro <gonzalopineiro.uni@gmail.com>
2025-05-14 10:08:02 -0300

    prueba

--- app/src/main/java/ayds/songinfo/home/view/SongDescriptionHelperImpl.kt
+++ app/src/main/java/ayds/songinfo/home/view/SongDescriptionHelperImpl.kt
@@ -24,36 +24,3 @@ internal class SongDescriptionHelperImpl : SongDescriptionHelper {
     }
 }
 
-    private fun SpotifySong.releaseDate() =
-        when(this.releaseDatePrecision) {
-            "day" -> {
-                val day = this.releaseDate.split("-").first()
-                val month = this.releaseDate.split("-")[1]
-                val year = this.releaseDate.split("-")[2]
-            }
-            "month" -> {
-                val year = this.releaseDate.split("-").first()
-                val month = this.releaseDate.split("-")[1]
-                "$(month.toInt().toMonthString()"
-            }
-
-        }
-
-    private fun Int.toMonthString() =
-        when (this) {
-            1 -> "January"
-            2 -> "February"
-            3 -> "March"
-            4 -> "April"
-            5 -> "May"
-            6 -> "June"
-            7 -> "July"
-            8 -> "August"
-            9 -> "September"
-            10 -> "October"
-            11 -> "November"
-            12 -> "December"
-            else -> ""
-        }
-
-private fun isLeapYear (n: Int) =
