717e1978d2ac039877e598de2a7d81b868d9f727
Gonzalo Piñeiro <gonzalopineiro.uni@gmail.com>
2025-05-14 10:09:52 -0300

    prueba

--- app/src/main/java/ayds/songinfo/home/model/entities/SpotifySong.kt
+++ app/src/main/java/ayds/songinfo/home/model/entities/SpotifySong.kt
@@ -7,11 +7,13 @@ sealed class Song {
         val artistName: String,
         val albumName: String,
         val releaseDate: String,
-        val releaseDatePrecision: String,
         val spotifyUrl: String,
         val imageUrl: String,
         var isLocallyStored: Boolean = false
-    ) : Song()
+    ) : Song() {
+
+        val year: String = releaseDate.split("-").first()
+    }
 
     object EmptySong : Song()
 }
